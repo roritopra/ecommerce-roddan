b2172085d039a2c9d2d21b090f8e6b4f6c731e35
Your Name <you@example.com>
2024-01-15 18:07:52 -0500

    [UPDATE] Updated last global config for daniel

--- src/user/pages/HomePage.jsx
+++ src/user/pages/HomePage.jsx
@@ -1,7 +1,23 @@
+import "./HomePage.css";
+
 export function HomePage() {
   return (
     <>
-      <h1>Hello Homepage</h1>
+      <section className="container-grid max-w-[1440px] px-5">
+        <div className="div1 rounded-[35px]">
+          <div className="flex items-center gap-5">
+            <img src="/icons/logo-icon.svg" alt="Logo icon" />
+            <p className="text-white text-sm font-poppins">Time for offers-24</p>
+          </div>
+          <h1 className="text-white text-9xl font-poppins max-w-[1440px]">JANUARY OFFERS, WE START THE YEAR</h1>
+        </div>
+        <div className="div2"></div>
+        <div className="div3"></div>
+        <div className="div4"></div>
+        <div className="div5"></div>
+        <div className="div6"></div>
+        <div className="div7"></div>
+      </section>
     </>
   );
 }
