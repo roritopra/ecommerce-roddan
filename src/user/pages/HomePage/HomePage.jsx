0b307f717f92f58052eaeacb8c658f4eeed85fee
Your Name <you@example.com>
2024-01-18 16:51:13 -0500

    [UPDATE] Added logoCarousel to component

--- src/user/pages/HomePage/HomePage.jsx
+++ src/user/pages/HomePage/HomePage.jsx
@@ -1,54 +1,12 @@
 import "./HomePage.css";
 import { HomeHeader } from "./components/HomeHeader/HomeHeader";
+import { LogoCarousel } from "./components/LogoCarousel/LogoCarousel";
 
 export function HomePage() {
   return (
     <>
       <HomeHeader />
-      
-      <section className="grid place-content-center max-w-[1440px] mx-auto px-5 my-28">
-      <div className="flex justify-center items-center h-full w-full overflow-hidden rounded-[35px] border border-[#d9d9d9]">
-        <div className="slider-items">
-          <img
-            src="icons/sony-logo.png"
-            alt="Sony logo"
-          />
-          <img
-            src="icons/jbl-logo.png"
-            alt="JBL logo"
-          />
-          <img
-            src="icons/huawei-logo.png"
-            alt="Huawei logo"
-          />
-          <img
-            src="icons/lenovo-logo.png"
-            alt="Lenovo logo"
-          />
-          <img
-            src="icons/xiaomi-logo.png"
-            alt="Xiaomi logo"
-          />
-          <img src="icons/samsung-logo.png" alt="Samsung logo" />
-          <img
-            src="icons/oculus-logo.png"
-            alt="Oculus logo"
-          />
-          <img
-            src="icons/apple-logo.png"
-            alt="Apple logo"
-          />
-          <img
-            src="icons/oppo-logo.png"
-            alt=""
-          />
-          <img
-            src="icons/asus-logo.png"
-            alt="Asus logo"
-          />      
-        </div>
-      </div>
-      </section>
+      <LogoCarousel />
     </>
   );
 }
