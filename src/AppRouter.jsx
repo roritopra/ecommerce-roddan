de26809ca7c365937800d362e277c4ee970f4b10
Your Name <you@example.com>
2024-01-22 00:47:14 -0500

    Add new SVG icon and ItemDetailsPage component

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import { NavBar } from "./user/components/NavBar/NavBar";
 import { HomePage } from "./user/pages/HomePage/HomePage";
 import { ShopPage } from "./user/pages/ShopPage/ShopPage";
+import { ItemDetailsPage } from "./user/pages/ItemDetailsPage/ItemDetailsPage";
 export function AppRouter() {
   return (
     <Routes>
       <Route path="/" element={<NavBar />}>
         <Route index element={<HomePage />} />
         <Route path="/shop" element={<ShopPage />} />
+        <Route path="/item" element={<ItemDetailsPage />} />
       </Route>
     </Routes>
   );
