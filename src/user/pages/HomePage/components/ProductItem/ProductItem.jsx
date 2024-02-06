8d8389caadf336959be8eef4927ba619218e6d83
roritopra <1193102598@u.icesi.edu.co>
2024-02-06 00:28:16 -0500

    [UPDATE] Ended hover for Productitem

--- src/user/pages/HomePage/components/ProductItem/ProductItem.jsx
+++ src/user/pages/HomePage/components/ProductItem/ProductItem.jsx
@@ -4,17 +4,19 @@ import { Button } from "@nextui-org/react";
 import { NavLink } from "react-router-dom";
 
 export function ProductItem({ image, name, price }) {
-  const [showButton, setShowButton] = useState(false);
+  const [hovered, setHovered] = useState(false);
 
   return (
     <>
       <div
-        className="relative mt-5 mb-28 cursor-pointer hover:filter hover:drop-shadow-2xl transition-all"
-        onMouseEnter={() => setShowButton(true)}
-        onMouseLeave={() => setShowButton(false)}
+        className={`item-container mt-5 mb-28 cursor-pointer hover:filter hover:drop-shadow-2xl transition-all ${
+          hovered ? "hovered" : ""
+        }`}
+        onMouseEnter={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)}
       >
         <NavLink to={"/shop"}>
-          <figure className="card flex justify-center h-full items-center rounded-[35px] max-w-[350px]">
+          <figure className="card flex justify-center h-full items-center rounded-[35px] max-w-[350px] relative">
             <img className="" src={image} alt="Items home" />
           </figure>
           <div className="pt-5">
@@ -23,18 +25,20 @@ export function ProductItem({ image, name, price }) {
               <span className="text-[#19191B] font-poppins text-[20px] font-semibold">
                 {price}
               </span>
-              {showButton && (
-                <div className="absolute bottom-0 right-0">
-                  <NavLink to={"/shop"}>
-                    <Button
-                      size="sm"
-                      className="bg-[#0081FE] text-[14px] font-poppins text-white rounded-full px-4 transition-all"
-                    >
-                      View more
-                    </Button>
-                  </NavLink>
-                </div>
-              )}
+              <div
+                className={`button-container absolute bottom-0 right-0 text-center ${
+                  hovered ? "visible" : ""
+                }`}
+              >
+                <NavLink to={"/shop"}>
+                  <Button
+                    size="sm"
+                    className="bg-[#0081FE] text-[14px] font-poppins text-white rounded-full px-4 transition-all"
+                  >
+                    View more
+                  </Button>
+                </NavLink>
+              </div>
             </div>
           </div>
         </NavLink>
