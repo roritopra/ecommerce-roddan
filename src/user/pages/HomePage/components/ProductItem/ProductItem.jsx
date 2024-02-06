9a8b122a17f1b0d31ed8573dcd8c18543328b9a5
roritopra <1193102598@u.icesi.edu.co>
2024-02-06 00:10:40 -0500

    [ADD] hover effect and view more button to ProductItem component

--- src/user/pages/HomePage/components/ProductItem/ProductItem.jsx
+++ src/user/pages/HomePage/components/ProductItem/ProductItem.jsx
@@ -1,28 +1,39 @@
+import  { useState } from "react";
 import "./ProductItem.css";
 import { Button } from "@nextui-org/react";
 import { NavLink } from "react-router-dom";
 
 export function ProductItem({ image, name, price }) {
+  const [showButton, setShowButton] = useState(false);
+
   return (
     <>
-      <div className="relative mt-5 mb-28 cursor-pointer hover:filter hover:drop-shadow-2xl transition-all">
+      <div
+        className="relative mt-5 mb-28 cursor-pointer hover:filter hover:drop-shadow-2xl transition-all"
+        onMouseEnter={() => setShowButton(true)}
+        onMouseLeave={() => setShowButton(false)}
+      >
         <figure className="card flex justify-center h-full items-center rounded-[35px] max-w-[350px]">
           <img className="" src={image} alt="Items home" />
         </figure>
-        <div className="mt-5">
+        <div className="pt-5">
           <p className="text-[#19191B] font-poppins text-[18px]">{name}</p>
-          <div className="flex justify-between items-center">
+          <div className="relative flex justify-between items-center">
             <span className="text-[#19191B] font-poppins text-[20px] font-semibold">
               {price}
             </span>
-            <NavLink to={"/shop"}>
-              <Button
-                size="sm"
-                className="bg-[#0081FE] text-[12px] font-poppins text-white rounded-full px-4"
-              >
-                Details
-              </Button>
-            </NavLink>
+            {showButton && (
+              <div className="absolute bottom-0 right-0">
+              <NavLink to={"/shop"}>
+                <Button
+                  size="sm"
+                  className="bg-[#0081FE] text-[14px] font-poppins text-white rounded-full px-4 transition-all"
+                >
+                  View more
+                </Button>
+              </NavLink>
+            </div>
+            )}
           </div>
         </div>
       </div>
