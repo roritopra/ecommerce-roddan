0526e23e8b39f32fda808ba4a5db23ecdcd6492d
Your Name <you@example.com>
2024-03-15 00:08:33 -0500

    [UPDATE] Added map array images in details page

--- src/user/pages/ItemDetailsPage/ItemDetailsPage.jsx
+++ src/user/pages/ItemDetailsPage/ItemDetailsPage.jsx
@@ -13,25 +13,7 @@ export function ItemDetailsPage() {
   const { productId } = useParams();
   const [count, setCount] = useState(1);
   const [product, setProduct] = useState(null);
-  const data = [
-    {
-      imgelink:
-        "https://images.unsplash.com/photo-1499696010180-025ef6e1a8f9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-    },
-    {
-      imgelink:
-        "https://images.unsplash.com/photo-1432462770865-65b70566d673?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
-    },
-    {
-      imgelink:
-        "https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80",
-    },
-    {
-      imgelink:
-        "https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80",
-    },
-  ];
-
+  
   const [active, setActive] = useState(
     "https://images.unsplash.com/photo-1499696010180-025ef6e1a8f9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
   );
@@ -182,21 +164,20 @@ export function ItemDetailsPage() {
 
           <div className="w-1/2">
             <div className="grid gap-4">
-              <div>
+              <div className={active === product.images[product.images.length - 1] ? "flex justify-center rounded-2xl" : "second-card flex justify-center rounded-2xl"}>
                 <Image
-                  isZoomed
                   className="h-auto w-full max-w-full rounded-[20px] object-cover object-center md:h-[480px]"
                   src={active}
                   alt=""
                 />
               </div>
               <div className="grid grid-cols-4 gap-4">
-                {data.map(({ imgelink }, index) => (
-                  <div key={index}>
+              {product.images.map((image, index) => (
+                  <div key={index} className={`${index === product.images.length - 1 ? 'w-full' : 'card'} flex justify-center rounded-2xl`}>
                     <img
-                      onClick={() => setActive(imgelink)}
-                      src={imgelink}
-                      className="h-28 max-w-full cursor-pointer rounded-xl object-cover object-center"
+                      onClick={() => setActive(image)}
+                      src={image}
+                      className="max-h-32 max-w-full cursor-pointer rounded-xl object-cover object-center"
                       alt="gallery-image"
                     />
                   </div>
