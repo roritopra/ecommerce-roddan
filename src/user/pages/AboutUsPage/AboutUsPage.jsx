c87820a0c3bd25a3a1435c720571721436a52c49
DANDIERU <1193080635@u.icesi.edu.co>
2024-03-26 23:46:06 -0500

    UPDATE

---
+++ src/user/pages/AboutUsPage/AboutUsPage.jsx
@@ -0,0 +1,16 @@
+import "./AboutUsPage.css";
+
+export function AboutUsPage() {
+  return (
+    <>
+      <section className="flex font-satoshi max-w-[1440px] mx-auto px-5">
+        <section className="big-div">
+          <div className="welcome-roddan"></div>
+          <div className="info-div"></div>
+          <div className="contact-us"></div>
+          <div className="our-blog"></div>
+        </section>
+      </section>
+    </>
+  );
+}
