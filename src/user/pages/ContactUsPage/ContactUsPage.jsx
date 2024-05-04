95a3c2eae9f4a3327f46984841b39a66add176e6
DANDIERU <1193080635@u.icesi.edu.co>
2024-05-04 12:03:28 -0500

    UPDATE

--- src/user/pages/ContactUsPage/ContactUsPage.jsx
+++ src/user/pages/ContactUsPage/ContactUsPage.jsx
@@ -1,6 +1,7 @@
 import "./ContactUsPage.css";
 import { Footer } from "../../components/Footer/Footer";
-import { Button } from "@nextui-org/react";
+
+import { EmailLibrary } from "../../components/EmailLibrary/EmailLibrary";
 
 export function ContactUsPage() {
   return (
@@ -30,62 +31,7 @@ export function ContactUsPage() {
             />
           </div>
         </section>
-        <form
-          action=""
-          className="form-area flex flex-col justify-center items-center w-1/2 bg-gray-100 gap-[21px] px-28 ml-[18px]"
-        >
-          <p className="text-[16px] font-[500] font-satoshi text-customColor">
-            Contact us
-          </p>
-          <h2 className="getintouch text-[45px] font-[600] font-satoshi ">
-            Get in touch
-          </h2>
-          <p className="text-[16px] font-[500] font-satoshi w-[414px] text-[#19191B] text-center ">
-            When, while lovely valley teems with vapour around me and meridian
-            the upper impenetrable.
-          </p>
-          <label className=" flex flex-col font-satoshi text-[14px] text-[#262E36] font-[500] w-full">
-            {" "}
-            Email
-            <input
-              type="email"
-              placeholder="example@email.com"
-              className="inputs border-none rounded-[6px] text-[14px] mt-1 text-[#4C5C6B] font-[400]"
-            />
-            <label className="text-[14px] text-[#656565] font-[400]">
-              {" "}
-              Enter full email
-            </label>
-          </label>
-          <label className=" flex flex-col font-satoshi text-[14px] text-[#262E36] font-[500] w-full">
-            {" "}
-            Phone number
-            <input
-              type="email"
-              placeholder="+57 1234567890"
-              className="inputs border-none rounded-[6px] text-[13px] mt-1 text-[#4C5C6B] font-[400]"
-            />
-            <label className="text-[14px] text-[#656565] font-[400]">
-              {" "}
-              Enter full phone number
-            </label>
-          </label>
-          <label className=" flex flex-col font-satoshi text-[14px] text-[#262E36] font-[500] w-full">
-            {" "}
-            Message
-            <textarea
-              placeholder="Your Message Here"
-              className="inputs border-none rounded-[6px] text-[13px] mt-1 text-[#4C5C6B] font-[400] "
-              rows={7}
-            ></textarea>
-          </label>
-          <Button
-            className="font-satoshi font-[400] text-[14px] px-16"
-            color="primary"
-          >
-            Submit
-          </Button>
-        </form>
+        <EmailLibrary></EmailLibrary>
       </section>
       <Footer />
     </main>
