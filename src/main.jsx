09dc35cbf80434a15af81e5c780888f50b4e1f2c
roritopra <1193102598@u.icesi.edu.co>
2024-07-07 18:51:36 -0500

    [UPDATE] change router dom to hash

--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { ThemeProvider } from "@material-tailwind/react";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { NextUIProvider } from "@nextui-org/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <ThemeProvider>
         <NextUIProvider>
           <App />
         </NextUIProvider>
       </ThemeProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
