7ce9c4d414ee9895bd2528a51e6969bfedbf7224
roritopra <1193102598@u.icesi.edu.co>
2024-07-09 21:36:32 -0500

    [UPDATE] Updated new navbar

--- src/user/components/NavBar/NavBar.jsx
+++ src/user/components/NavBar/NavBar.jsx
@@ -1,7 +1,7 @@
 import { NavLink } from "react-router-dom";
-import { Button } from "@nextui-org/react";
 import { Outlet } from "react-router-dom";
 import { menuLinks } from "./menuLinks";
+import CartIcon from "/icons/cart-icon.svg";
 
 export function NavBar() {
   return (
@@ -11,27 +11,30 @@ export function NavBar() {
           <NavLink to={"/"}>
             <img src="/icons/logo-nav.svg" alt="Roddan Logo" />
           </NavLink>
-          {menuLinks.map((menu, index) => (
-            <NavLink
-              key={index}
-              to={menu.path}
-              className={({ isActive }) => {
-                return `font-satoshi text-base ${
-                  isActive ? "text-[#0081FE]" : "text-[#9090A3]"
-                }`;
-              }}
-            >
-              {menu.text}
-            </NavLink>
-          ))}
         </nav>
-        <nav className="flex items-center font-medium gap-16 text-base font-satoshi">
-          <NavLink to={"/login"} className="text-[#9090A3]">
-            Login
-          </NavLink>
-          <NavLink to={"/register"}>
-            <Button size="md" radius="sm" className="bg-[#0081FE] px-7 font-medium text-white font-satoshi text-base">Register</Button>
-          </NavLink>
+        <nav className="flex items-center font-medium gap-8">
+          <div className="flex items-center font-medium gap-16 text-base font-satoshi">
+            {menuLinks.map((menu, index) => (
+              <NavLink
+                key={index}
+                to={menu.path}
+                className={({ isActive }) => {
+                  return `font-satoshi text-base ${
+                    isActive ? "text-[#0081FE]" : "text-[#9090A3]"
+                  }`;
+                }}
+              >
+                {menu.text}
+              </NavLink>
+            ))}
+          </div>
+          <div className="h-5 w-[1px] bg-[#9090A3]"></div>
+          <div className="relative cursor-pointer">
+            <img src={CartIcon} alt="Cart Icon" />
+            <div className="w-[14px] h-[14px] bg-[#0081FE] font-satoshi text-white font-medium text-xs absolute rounded-full flex justify-center items-center top-[-2px] left-[9px]">
+              3
+            </div>
+          </div>
         </nav>
       </header>
       <Outlet />
